114775e438b6e433d945e4fd92a9507ad3b3227e
mattrondel <109989470+mattrondel@users.noreply.github.com>
2022-07-29 12:41:26 -0700

    Create Activate windows server offline.ps1

---
+++ Activate windows server offline.ps1
@@ -0,0 +1,12 @@
+set-location c:\windows\system32
+write-host "Welcome to the windows server activation script"
+forgroundcolor yellow
+$hostname = read-host "enter the computer name you would like to activate"
+if($hostname -eq $env:COMPUTERNAME)
+	{
+		cscript slmgr.vps /ipk 123-ABC-windows-key-here
+	}
+else
+	{
+		cscript slmgr.vbs $Hostname /ipk 123-ABC-windows-key-here
+	}
