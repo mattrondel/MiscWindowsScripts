77ae56f6b53862e6b61e6b3890807296d98b710d
mattrondel <109989470+mattrondel@users.noreply.github.com>
2022-07-29 13:08:26 -0700

    Update Activate Multiple Windows 10 Host Offline.ps1

--- Activate Multiple Windows 10 Host Offline.ps1
+++ Activate Multiple Windows 10 Host Offline.ps1
@@ -1,1 +1,10 @@
-
+#Multiple Windows 10 hosts, add hostnames to external file: computers.txt
+Write-host "Welcome to the Windows 10 activation script" -ForegroundColor Yellow
+Start-process notepad.exe -ArgumentList .\Computers.txt -Wait
+$Computers = Get-Content .\Computers.txt
+ForEach($Computer in $Computers) {
+		Write-Host "Attempting to Activate $Computer" -ForegroundColor Cyan
+		cscript c:\Windows\System32\slmgr.vbs $Computer /ipk 4
+		1234-ABCD-WINDOWS-10-KEY-HERE
+}
+Pause
